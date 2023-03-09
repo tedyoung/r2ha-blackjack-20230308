d81f903b7a58eebc50da025ceb682900f964da7f
Ted M. Young <tedyoung@gmail.com>
2023-03-08 16:11:19 -0800

    LAB #4: Prepare Refactoring: introduce Deck as parameter, puts the burden of creating a Deck on the caller

---
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameTest.java
@@ -0,0 +1,16 @@
+package com.jitterted.ebp.blackjack.domain;
+
+class GameTest {
+
+//    @Test
+//    void playerHitsAndGoesBustThenOutcomeIsPlayerLoses() {
+//        Game game = new Game();
+//        game.initialDeal();
+//
+//        game.playerHits();
+//
+//        assertThat(game.determineOutcome())
+//                .isEqualTo("You Busted, so you lose.  💸");
+//    }
+
+}
