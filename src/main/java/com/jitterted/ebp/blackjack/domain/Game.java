1350c46d9ab8a9763b70123ead38a0b33713ac3e
Ted M. Young <tedyoung@gmail.com>
2023-03-09 15:18:46 -0800

    LAB #8A+B: Refactored to "funnel" Player Done State Change through a single method.

--- src/main/java/com/jitterted/ebp/blackjack/domain/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/domain/Game.java
@@ -22,15 +22,6 @@ public class Game {
         this.gameMonitor = gameMonitor;
     }
 
-    public void initialDeal() {
-        dealRoundOfCards();
-        dealRoundOfCards();
-        if (playerHand.isBlackjack()) {
-            playerDone = true;
-            gameMonitor.roundCompleted(this);
-        }
-    }
-
     private void dealRoundOfCards() {
         // why: players first because this is the rule
         playerHand.drawFrom(deck);
@@ -78,18 +69,27 @@ public class Game {
         return dealerHand;
     }
 
+    public void initialDeal() {
+        dealRoundOfCards();
+        dealRoundOfCards();
+        updatePlayerDoneStateTo(playerHand.isBlackjack());
+    }
+
     public void playerHits() {
         playerHand.drawFrom(deck);
-        playerDone = playerHand.isBusted();
-        if (playerDone) {
-            gameMonitor.roundCompleted(this);
-        }
+        updatePlayerDoneStateTo(playerHand.isBusted());
     }
 
     public void playerStands() {
-        playerDone = true;
         dealerTurn();
-        gameMonitor.roundCompleted(this);
+        updatePlayerDoneStateTo(true);
+    }
+
+    private void updatePlayerDoneStateTo(boolean isPlayerDone) {
+        playerDone = isPlayerDone;
+        if (playerDone) {
+            gameMonitor.roundCompleted(this);
+        }
     }
 
     public boolean isPlayerDone() {
